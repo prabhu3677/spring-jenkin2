87b3737a351f01c785d08e47ca41c4187138a9b6
Prabhudutta Sarangi <prabhusarangi.94@gmail.com>
2020-05-28 02:38:31 +0530

    second commit

--- src/test/java/com/example/jenkin/api/SpringJenkin2ApplicationTests.java
+++ src/test/java/com/example/jenkin/api/SpringJenkin2ApplicationTests.java
@@ -19,6 +19,7 @@ public class SpringJenkin2ApplicationTests {
 	public void contextLoads() {
 	
 		logger.info("Test case executing......");
+		logger.info("Test case executing second log statement......");
 		assertEquals(true, true);
 		
 	}
