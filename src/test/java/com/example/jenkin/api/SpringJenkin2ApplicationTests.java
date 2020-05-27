f42e8ff120416ab3f6d2b4787ea9df7790f38b99
Prabhudutta Sarangi <prabhusarangi.94@gmail.com>
2020-05-28 02:14:15 +0530

    first commit

---
+++ src/test/java/com/example/jenkin/api/SpringJenkin2ApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.jenkin.api;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJenkin2ApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkin2ApplicationTests.class);
+
+	@Test
+	public void contextLoads() {
+	
+		logger.info("Test case executing......");
+		assertEquals(true, true);
+		
+	}
+
+}
